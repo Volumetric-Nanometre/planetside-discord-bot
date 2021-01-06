23ec0511992faba5818c740011de5187ebe62f3e
Michael O'Donnell <mo14776@bristol.ac.uk>
2021-01-06 20:10:45 +0000

    Improve and expand soberdogs signup - allow sign up to prevent reactions no relevent - allow sign up to prevent multiple attempts - add names to list as required

--- src/opsignup.py
+++ src/opsignup.py
@@ -14,14 +14,15 @@ class OpSignUp(commands.Cog):
         self.soberdogsActive = False
         self.armourdogsActive = False
         self.soberdogMessageText = ""
-        self.soberdogMessageID = None
+        self.soberdogMessageHandlerID = None
         self.signUpChannelName ='✍-sign-up'
-        self.signupPoll = False
-        self.soberReactions={':Icon_Heavy_Assault:': 'Heavy',':Icon_Combat_Medic:' : 'Medic', ':Icon_Infiltrator:' : 'Infiltrator', ':Icon_Light_Assault:' : 'Light assault', ':Icon_Engineer:' : 'Engineer'}
-        
+        self.signUpChannel = None
+        self.soberMembers = []
+        self.soberMemberText = {}
+        self.ignoreRemove = False
+        self.soberReactions={'<:Icon_Heavy_Assault:795726910344003605>': 'Heavy','<:Icon_Combat_Medic:795726867960692806>' : 'Medic', '<:Icon_Infiltrator:795726922264215612>' : 'Infiltrator', '<:Icon_Light_Assault:795726936759468093>' : 'Light assault', '<:Icon_Engineer:795726888763916349>' : 'Engineer'}
+        self.bot = bot
         super().__init__()
-
-        
         
     async def locate_sign_up(self,ctx):
         print('Lookup signup channel')
@@ -36,25 +37,122 @@ class OpSignUp(commands.Cog):
                 print('Channel Found')
                 return channel
         print('Lookup complete')
+        
+        
+    async def sober_dogs_add(self,payload):
+        
+        messageText  = self.soberdogMessageText
+        message = await self.signUpChannel.fetch_message(self.soberdogMessageHandlerID)
+        if  str(payload.emoji) in self.soberReactions.keys() and str(payload.user_id) not in self.soberMembers:
+
+            self.soberMembers.append(str(payload.user_id))
+            self.soberMemberText.update({str(payload.user_id):f'\n{self.soberReactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
+
+
+
+            for player in self.soberMemberText.values():
+                messageText = messageText + str(player)
+
+
+            print(messageText)
+
+            await message.edit(content=messageText)
+
+            print('Reaction accepted')
+        else:
+            self.ignoreRemove = True
+            await message.remove_reaction(payload.emoji,payload.member)
+            print('Reaction removed')
+       
+    
+    async def sober_dogs_remove(self,payload):
+            
+            if self.ignoreRemove:
+                self.ignoreRemove = False
+                return
+            
+            if str(payload.emoji) in self.soberReactions.keys():
+                message = await self.signUpChannel.fetch_message(self.soberdogMessageHandlerID)
+                messageText  = self.soberdogMessageText
+                del self.soberMemberText[str(payload.user_id)]
+                self.soberMembers.remove(str(payload.user_id))
+                for player in self.soberMemberText.values():
+                    messageText = messageText + str(player)
+                await message.edit(content=messageText)
+            
+        
+        
     
     @commands.command(name='ps2-sober-signup')
     async def sober_dog_signup(self,ctx):
-        channel = await OpSignUp.locate_sign_up(self,ctx)
+        self.signUpChannel = await OpSignUp.locate_sign_up(self,ctx)
         with open('messages/soberdogs.txt','r') as f:
-            self.soberdogMessage = f.read()
+            self.soberdogMessageText = f.read()
         
-        messageHandler = await channel.send(self.soberdogMessage)
+        messageHandler = await self.signUpChannel.send(self.soberdogMessageText)
         self.soberdogMessageHandlerID = messageHandler.id
-        self.signupPoll = True
-        while(self.signupPoll):
-            print('Yay!')
-            await asyncio.sleep(5)
-            print(f'Yay- awake! {self.soberdogMessageHandlerID}')
-            message = await ctx.fetch_message(self.soberdogMessageHandlerID)
-            print('yes')
-            reactions = message.reactions
-            for reaction in reactions:
-                print(reaction.emoji) 
+        print('Message sent')
+    
+    @commands.command(name='ps2-sober-signup-reset')
+    async def sober_dog_signup_reset(self,ctx):
+        await message.delete()
+        
+        self.soberdogsActive = False
+        self.armourdogsActive = False
+        self.soberdogMessageText = ""
+        self.soberdogMessageHandlerID = None
+        self.signUpChannelName ='✍-sign-up'
+        self.signUpChannel = None
+        self.soberMembers = []
+        self.soberMemberText = {}
+        self.ignoreRemove = False
+    
+ 
+    
+    @commands.Cog.listener('on_raw_reaction_remove') 
+    async def react_remove_sign_up_check(self,payload):
+        print('remove reaction caught')       
+    
+        if not self.signUpChannel:
+            return
+        if payload.channel_id != self.signUpChannel.id:
+            print('not in sign-up')
+            return
+        if payload.message_id == self.soberdogMessageHandlerID:
+            print('Soberdogs remove react')
+            await OpSignUp.sober_dogs_remove(self,payload)
+
+            
+        else:
+            print('Not sober')
+    
+        print('removed in sign-up')
+        
+        
+    @commands.Cog.listener('on_raw_reaction_add') 
+    async def react_sign_up_check(self,payload):
+        
+        print('reaction caught') 
+        if not self.signUpChannel:
+            return
+        if payload.channel_id != self.signUpChannel.id:
+            print('not in sign-up')
+            return
+        if payload.message_id == self.soberdogMessageHandlerID:
+            print('Soberdogs react')
+            await OpSignUp.sober_dogs_add(self,payload)
+
+            
+        else:
+            print('Not sober')
+    
+        print('in sign-up')
+        
+        
+        
+        
+    
+    
             
                 
             
