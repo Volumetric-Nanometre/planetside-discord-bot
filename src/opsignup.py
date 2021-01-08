67660bafedc526d16f5d3298df89d882362ad3b2
Michael O'Donnell <mo14776@bristol.ac.uk>
2021-01-08 21:15:49 +0000

    Refactor opsignup
    - Refactor to allow better use of OOP stlye programming

--- src/opsignup.py
+++ src/opsignup.py
@@ -33,9 +33,155 @@ class OpSignUp(commands.Cog):
         self.bastionMemberText = {}
         self.bastionReactions={'<:NC:727306728470872075>':'NC'}
         self.ignoreBastionRemove = False
+        self.soberObj = {}
+        self.armourObj = {}
+        self.bastionObj = {}
         self.bot = bot
         super().__init__()
         
+
+    @commands.Cog.listener('on_raw_reaction_remove') 
+    async def react_remove_sign_up_check(self,payload):
+        print('remove reaction caught')   
+           
+        if payload.message_id in self.soberObj:
+            print('Soberdogs remove react')
+            sober = self.soberObj[payload.message_id]
+            await OpSignUp.generic_react_remove(sober,payload)
+        elif payload.message_id in self.armourObj:
+            print('Armourdogs remove react')
+            armour = self.armourObj[payload.message_id]
+            await OpSignUp.generic_react_remove(armour,payload)
+        elif payload.message_id in self.bastionObj:
+            print('Bastion remove react')
+            bastion = self.bastionObj[payload.message_id]
+            await OpSignUp.generic_react_remove(bastion,payload)
+        else:
+            pass
+        
+        print('Complete')
+
+        
+        
+    @commands.Cog.listener('on_raw_reaction_add') 
+    async def react_sign_up_check(self,payload):
+        
+        print('reaction caught') 
+        if payload.message_id in self.soberObj:
+            print('Soberdogs remove react')
+            sober = self.soberObj[payload.message_id]
+            await OpSignUp.generic_react_add(sober,payload)
+        elif payload.message_id in self.armourObj:
+            print('Soberdogs remove react')
+            armour = self.armourObj[payload.message_id]
+            await OpSignUp.generic_react_add(armour,payload)
+        elif payload.message_id in self.bastionObj:
+            print('Soberdogs remove react')
+            bastion = self.bastionObj[payload.message_id]
+            await OpSignUp.generic_react_add(bastion,payload)
+            
+        else:
+            pass
+    
+        print('Complete')
+        
+        
+    @commands.Cog.listener('on_raw_message_delete') 
+    async def generic_reset_check(self,payload):
+        
+        if payload.message_id in self.soberObj:
+            del self.soberObj[payload.message_id]
+        elif payload.message_id in self.armourObj:
+            del self.armourObj[payload.message_id]
+        elif payload.message_id in self.bastionObj:
+            del self.bastionObj[payload.message_id]
+            print(self.bastionObj)
+        else:
+            pass
+    
+        print('Complete')         
+    
+    
+    @commands.command(name='ps2-signup')
+    async def generic_signup(self,ctx,signup):
+        
+        channel = await OpSignUp.locate_sign_up(self,ctx)
+        
+        if signup == 'soberdogs':
+            sober=SoberDogs(channel)
+            print('Soberdogs instantiated')
+            await sober.send_message()
+            print(f'Soberdogs messgae sent {sober.messageHandlerID}')
+            self.soberObj.update( {sober.messageHandlerID : sober})
+            print('Soberdogs added to dictionary')
+        elif signup == 'armourdogs':
+            armour=ArmourDogs(channel)
+            print('Armourdogs instantiated')
+            await armour.send_message()
+            print(f'Armourdogs messgae sent {armour.messageHandlerID}')
+            self.armourObj.update( {armour.messageHandlerID : armour})
+            print('Armourdogs added to dictionary')
+        elif signup == 'bastion':
+            bastion=Bastion(channel)
+            print('Bastion instantiated')
+            await bastion.send_message()
+            print(f'Bastion messgae sent {bastion.messageHandlerID}')
+            self.bastionObj.update( {bastion.messageHandlerID : bastion})
+            print('Bastion added to dictionary')
+        else:
+            print('Sign up type does not exist')
+            
+        print('Complete')
+    
+    
+    async def generic_react_add(self,payload):
+        
+        messageText  = self.messageText
+        message = await self.signUpChannel.fetch_message(self.messageHandlerID)
+        
+        
+        
+        if  str(payload.emoji) in self.reactions.keys() and str(payload.user_id) not in self.members:
+            
+            self.members.append(str(payload.user_id))
+            
+            if payload.member.nick != None:
+                self.memberText.update({str(payload.user_id):f'\n{self.reactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
+            else:
+                self.memberText.update({str(payload.user_id):f'\n{self.reactions[str(payload.emoji)]} - @{str(payload.member.name)}'})
+                
+            
+            for player in self.memberText.values():
+                messageText = messageText + str(player)
+
+
+            print(messageText)
+
+            await message.edit(content=messageText)
+
+            print('Reaction accepted')
+        else:
+            self.ignoreRemove = True
+            await message.remove_reaction(payload.emoji,payload.member)
+            print('Reaction removed')
+              
+            
+    async def generic_react_remove(self,payload):
+            
+        if self.ignoreRemove:
+            self.ignoreRemove = False
+            return
+
+        if str(payload.emoji) in self.reactions.keys():
+            message = await self.signUpChannel.fetch_message(self.messageHandlerID)
+            messageText  = self.messageText
+            del self.memberText[str(payload.user_id)]
+            self.members.remove(str(payload.user_id))
+            for player in self.memberText.values():
+                messageText = messageText + str(player)
+            await message.edit(content=messageText)
+            
+            
     async def locate_sign_up(self,ctx):
         print('Lookup signup channel')
         try:
@@ -51,260 +197,69 @@ class OpSignUp(commands.Cog):
         print('Lookup complete')
         
         
-    async def sober_dogs_add(self,payload):
         
-        messageText  = self.soberdogMessageText
-        message = await self.signUpChannel.fetch_message(self.soberdogMessageHandlerID)
-        
-        
-        
-        if  str(payload.emoji) in self.soberReactions.keys() and str(payload.user_id) not in self.soberMembers:
-            
-            self.soberMembers.append(str(payload.user_id))
-            
-            if payload.member.nick != None:
-                self.soberMemberText.update({str(payload.user_id):f'\n{self.soberReactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
-            else:
-                self.soberMemberText.update({str(payload.user_id):f'\n{self.soberReactions[str(payload.emoji)]} - @{str(payload.member.name)}'})
-                
-            
-            for player in self.soberMemberText.values():
-                messageText = messageText + str(player)
-
-
-            print(messageText)
-
-            await message.edit(content=messageText)
-
-            print('Reaction accepted')
-        else:
-            self.ignoreSoberRemove = True
-            await message.remove_reaction(payload.emoji,payload.member)
-            print('Reaction removed')
-       
+class SoberDogs:
     
-    async def sober_dogs_remove(self,payload):
-            
-        if self.ignoreSoberRemove:
-            self.ignoreSoberRemove = False
-            return
+    def __init__(self,channel):
+        self.signUpChannel = channel
+        self.messageText = SoberDogs.get_message()
+        self.messageHandlerID = None
+        self.members = []
+        self.memberText = {}
+        self.ignoreRemove = False
+        self.reactions={'<:Icon_Heavy_Assault:795726910344003605>': 'Heavy','<:Icon_Combat_Medic:795726867960692806>' : 'Medic', '<:Icon_Infiltrator:795726922264215612>' : 'Infiltrator', '<:Icon_Light_Assault:795726936759468093>' : 'Light assault', '<:Icon_Engineer:795726888763916349>' : 'Engineer'}
+   
 
-        if str(payload.emoji) in self.soberReactions.keys():
-            message = await self.signUpChannel.fetch_message(self.soberdogMessageHandlerID)
-            messageText  = self.soberdogMessageText
-            del self.soberMemberText[str(payload.user_id)]
-            self.soberMembers.remove(str(payload.user_id))
-            for player in self.soberMemberText.values():
-                messageText = messageText + str(player)
-            await message.edit(content=messageText)
-            
-        
-        
-    
-    @commands.command(name='ps2-sober-signup')
-    async def sober_dog_signup(self,ctx):
-        self.signUpChannel = await OpSignUp.locate_sign_up(self,ctx)
+    def get_message():
         with open('messages/soberdogs.txt','r') as f:
-            self.soberdogMessageText = f.read()
+            messageText = f.read()
+            return messageText
+    
+    async def send_message(self):
+        messageHandler = await self.signUpChannel.send(self.messageText)
+        self.messageHandlerID = messageHandler.id
         
-        messageHandler = await self.signUpChannel.send(self.soberdogMessageText)
-        self.soberdogMessageHandlerID = messageHandler.id
-        print('Message sent')
-    
-    @commands.command(name='ps2-sober-signup-reset')
-    async def sober_dog_signup_reset(self,ctx):
-        message = await self.signUpChannel.fetch_message(self.soberdogMessageHandlerID)
-        await message.delete()
         
-        self.soberdogMessageText = ""
-        self.soberdogMessageHandlerID = None
-        self.soberMembers = []
-        self.soberMemberText = {}
-        self.ignoreSoberRemove = False
+        
+class ArmourDogs:
     
- 
-    
-    @commands.Cog.listener('on_raw_reaction_remove') 
-    async def react_remove_sign_up_check(self,payload):
-        print('remove reaction caught')       
-    
-        if not self.signUpChannel:
-            return
-        if payload.channel_id != self.signUpChannel.id:
-            print('not in sign-up')
-            return
-        if payload.message_id == self.soberdogMessageHandlerID:
-            print('Soberdogs remove react')
-            await OpSignUp.sober_dogs_remove(self,payload)
-        if payload.message_id == self.bastionMessageHandlerID:
-            print('Bastion remove react')
-            await OpSignUp.bastion_remove(self,payload)
+    def __init__(self,channel):
+        self.signUpChannel = channel
+        self.messageText = ArmourDogs.get_message()
+        self.messageHandlerID = None
+        self.members = []
+        self.memberText = {}
+        self.ignoreRemove = False
+        self.reactions={'<:Icon_Vanguard:795727955896565781>':'Vanguard','<:ps2flash:795726333455237121>': 'Flash','<:Icon_Sunderer:795727911549272104>': 'Sunderer','<:Icon_Lightning:795727852875677776>':'Lightning','<:Icon_ANT:795727784239824896>' : 'ANT','<:Icon_Harasser:795727814220840970>' : 'Harasser' }
+   
 
-            
-        else:
-            print('Not sober')
-    
-        print('removed in sign-up')
-        
-        
-    @commands.Cog.listener('on_raw_reaction_add') 
-    async def react_sign_up_check(self,payload):
-        
-        print('reaction caught') 
-        if not self.signUpChannel:
-            return
-        if payload.channel_id != self.signUpChannel.id:
-            print('not in sign-up')
-            return
-        if payload.message_id == self.soberdogMessageHandlerID:
-            print('Soberdogs react')
-            await OpSignUp.sober_dogs_add(self,payload)
-        if payload.message_id == self.armourdogMessageHandlerID:
-            print('ArmourDogs react')
-            await OpSignUp.armour_dogs_add(self,payload)
-        if payload.message_id == self.bastionMessageHandlerID:
-            print('Bastion react')
-            await OpSignUp.bastion_add(self,payload)
-
-            
-        else:
-            print('Not sober')
-    
-        print('in sign-up')
-        
-
-    @commands.command(name='ps2-armour-signup')
-    async def armour_dog_signup(self,ctx):
-        self.signUpChannel = await OpSignUp.locate_sign_up(self,ctx)
+    def get_message():
         with open('messages/armourdogs.txt','r') as f:
-            self.armourdogMessageText = f.read()
-        
-        messageHandler = await self.signUpChannel.send(self.armourdogMessageText)
-        self.armourdogMessageHandlerID = messageHandler.id
-        print('Message sent')
+            messageText = f.read()
+            return messageText
     
-    @commands.command(name='ps2-armour-signup-reset')
-    async def armour_dog_signup_reset(self,ctx):
-        message = await self.signUpChannel.fetch_message(self.armourdogMessageHandlerID)
-        await message.delete()
+    async def send_message(self):
+        messageHandler = await self.signUpChannel.send(self.messageText)
+        self.messageHandlerID = messageHandler.id
         
-        self.armourdogMessageText = ""
-        self.armourdogMessageHandlerID = None
-        self.armourMembers = []
-        self.armourMemberText = {}
-        self.ignoreArmourRemove = False
         
+class Bastion:
     
-    async def armour_dogs_add(self,payload):
-        
-        messageText  = self.armourdogMessageText
-        message = await self.signUpChannel.fetch_message(self.armourdogMessageHandlerID)
-        
-        if  str(payload.emoji) in self.armourReactions.keys() and str(payload.user_id) not in self.armourMembers:
-            
-            self.armourMembers.append(str(payload.user_id))
-            
-            if payload.member.nick != None:
-                self.armourMemberText.update({str(payload.user_id):f'\n{self.armourReactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
-            else:
-                self.armourMemberText.update({str(payload.user_id):f'\n{self.armourReactions[str(payload.emoji)]} - @{str(payload.member.name)}'})
-                
-            
-            for player in self.armourMemberText.values():
-                messageText = messageText + str(player)
+    def __init__(self,channel):
+        self.signUpChannel = channel
+        self.messageText = Bastion.get_message()
+        self.messageHandlerID = None
+        self.members = []
+        self.memberText = {}
+        self.ignoreRemove = False
+        self.reactions={'<:NC:727306728470872075>':'NC'}
+   
 
-
-            print(messageText)
-
-            await message.edit(content=messageText)
-
-            print('Reaction accepted')
-        else:
-            self.ignoreArmourRemove = True
-            await message.remove_reaction(payload.emoji,payload.member)
-            print('Reaction removed')
-            
-            
-    async def armour_dogs_remove(self,payload):
-            
-        if self.ignoreArmourRemove:
-            self.ignoreArmourRemove = False
-            return
-
-        if str(payload.emoji) in self.armourReactions.keys():
-            message = await self.signUpChannel.fetch_message(self.armourdogMessageHandlerID)
-            messageText  = self.armourdogMessageText
-            del self.armourMemberText[str(payload.user_id)]
-            self.armourMembers.remove(str(payload.user_id))
-            for player in self.armourMemberText.values():
-                messageText = messageText + str(player)
-            await message.edit(content=messageText)
-            
-    @commands.command(name='ps2-bastion-signup')
-    async def bastion_signup(self,ctx):
-        self.signUpChannel = await OpSignUp.locate_sign_up(self,ctx)
+    def get_message():
         with open('messages/bastion.txt','r') as f:
-            self.bastionMessageText = f.read()
-        
-        messageHandler = await self.signUpChannel.send(self.bastionMessageText)
-        self.bastionMessageHandlerID = messageHandler.id
-        print('Message sent')
+            messageText = f.read()
+            return messageText
     
-    @commands.command(name='ps2-bastion-signup-reset')
-    async def bastion_signup_reset(self,ctx):
-        message = await self.signUpChannel.fetch_message(self.bastionMessageHandlerID)
-        await message.delete()
-        
-        self.bastionMessageText = ""
-        self.bastionMessageHandlerID = None
-        self.bastionMembers = []
-        self.bastionMemberText = {}
-        self.ignoreBastionRemove = False
-        
-        
-    async def bastion_remove(self,payload):
-            
-        if self.ignoreBastionRemove:
-            self.ignoreBastionRemove = False
-            return
-
-        if str(payload.emoji) in self.bastionReactions.keys():
-            message = await self.signUpChannel.fetch_message(self.bastionMessageHandlerID)
-            messageText  = self.bastionMessageText
-            del self.bastionMemberText[str(payload.user_id)]
-            self.bastionMembers.remove(str(payload.user_id))
-            for player in self.bastionMemberText.values():
-                messageText = messageText + str(player)
-            await message.edit(content=messageText)
-            
-            
-    async def bastion_add(self,payload):
-        
-        messageText  = self.bastionMessageText
-        message = await self.signUpChannel.fetch_message(self.bastionMessageHandlerID)
-        
-        print(payload.emoji)
-        
-        if  str(payload.emoji) in self.bastionReactions.keys() and str(payload.user_id) not in self.bastionMembers:
-            
-            self.bastionMembers.append(str(payload.user_id))
-            
-            if payload.member.nick != None:
-                self.bastionMemberText.update({str(payload.user_id):f'\n{self.bastionReactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
-            else:
-                self.bastionMemberText.update({str(payload.user_id):f'\n{self.bastionReactions[str(payload.emoji)]} - @{str(payload.member.name)}'})
-                
-            
-            for player in self.bastionMemberText.values():
-                messageText = messageText + str(player)
-
-
-            print(messageText)
-            
-            await message.edit(content=messageText)
-
-            print('Reaction accepted')
-        else:
-            self.ignoreBastionRemove = True
-            await message.remove_reaction(payload.emoji,payload.member)
-            print('Reaction removed')
+    async def send_message(self):
+        messageHandler = await self.signUpChannel.send(self.messageText)
+        self.messageHandlerID = messageHandler.id
