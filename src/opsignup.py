f0d13c189481a250eef9c0cdd234abc9413da99c
Michael O'Donnell <mo14776@bristol.ac.uk>
2021-01-10 13:49:12 +0000

    Finalise signup sheets - add date allowance - add max roles - add mentioning

--- src/opsignup.py
+++ src/opsignup.py
@@ -83,28 +83,28 @@ class OpSignUp(commands.Cog):
     
     
     @commands.command(name='ps2-signup')
-    async def generic_signup(self,ctx,signup):
+    async def generic_signup(self,ctx,signup,date):
         
         channel = await OpSignUp.locate_sign_up(self,ctx)
         
         if signup == 'soberdogs':
             sober=SoberDogs(channel)
             print('Soberdogs instantiated')
-            await sober.send_message()
+            await sober.send_message(ctx,date)
             print(f'Soberdogs messgae sent {sober.messageHandlerID}')
             self.soberObj.update( {sober.messageHandlerID : sober})
             print('Soberdogs added to dictionary')
         elif signup == 'armourdogs':
             armour=ArmourDogs(channel)
             print('Armourdogs instantiated')
-            await armour.send_message()
+            await armour.send_message(ctx,date)
             print(f'Armourdogs messgae sent {armour.messageHandlerID}')
             self.armourObj.update( {armour.messageHandlerID : armour})
             print('Armourdogs added to dictionary')
         elif signup == 'bastion':
             bastion=Bastion(channel)
             print('Bastion instantiated')
-            await bastion.send_message()
+            await bastion.send_message(ctx,date)
             print(f'Bastion messgae sent {bastion.messageHandlerID}')
             self.bastionObj.update( {bastion.messageHandlerID : bastion})
             print('Bastion added to dictionary')
@@ -121,15 +121,16 @@ class OpSignUp(commands.Cog):
         
         
         
-        if  str(payload.emoji) in self.reactions.keys() and str(payload.user_id) not in self.members:
+        if  str(payload.emoji) in self.reactions.keys() and str(payload.user_id) not in self.members and not OpSignUp.react_max(self,payload):
+            
+            
+            
+            
+            OpSignUp.update_react_num(self,payload,'add')
             
             self.members.append(str(payload.user_id))
             
-            if payload.member.nick != None:
-                self.memberText.update({str(payload.user_id):f'\n{self.reactions[str(payload.emoji)]} - @{str(payload.member.nick)}'})
-            else:
-                self.memberText.update({str(payload.user_id):f'\n{self.reactions[str(payload.emoji)]} - @{str(payload.member.name)}'})
-                
+            self.memberText.update({str(payload.user_id):f'\n{self.reactions[str(payload.emoji)]} - {str(payload.member.mention)}'}) 
             
             for player in self.memberText.values():
                 messageText = messageText + str(player)
@@ -153,10 +154,12 @@ class OpSignUp(commands.Cog):
             return
 
         if str(payload.emoji) in self.reactions.keys():
+            
             message = await self.signUpChannel.fetch_message(self.messageHandlerID)
             messageText  = self.messageText
             del self.memberText[str(payload.user_id)]
             self.members.remove(str(payload.user_id))
+            OpSignUp.update_react_num(self,payload,'remove')
             for player in self.memberText.values():
                 messageText = messageText + str(player)
             await message.edit(content=messageText)
@@ -176,6 +179,27 @@ class OpSignUp(commands.Cog):
                 return channel
         print('Lookup complete')
         
+    def react_max(self,payload):
+        
+        if self.maxReact[str(payload.emoji)][0] < 0:
+            return False
+        elif self.maxReact[str(payload.emoji)][1] < self.maxReact[str(payload.emoji)][0]:
+            return False
+        else:
+            return True
+        
+    def update_react_num(self,payload,operation=str):
+        
+        maxReact = self.maxReact[str(payload.emoji)][0]
+        current = self.maxReact[str(payload.emoji)][1]
+        if operation == 'add':
+            current = current + 1
+        elif operation == 'remove':
+            current = current - 1
+
+        self.maxReact.update({str(payload.emoji):[maxReact,current]})
+            
+        
         
         
 class SoberDogs:
@@ -188,14 +212,24 @@ class SoberDogs:
         self.memberText = {}
         self.ignoreRemove = False
         self.reactions={'<:Icon_Heavy_Assault:795726910344003605>': 'Heavy','<:Icon_Combat_Medic:795726867960692806>' : 'Medic', '<:Icon_Infiltrator:795726922264215612>' : 'Infiltrator', '<:Icon_Light_Assault:795726936759468093>' : 'Light assault', '<:Icon_Engineer:795726888763916349>' : 'Engineer'}
-   
-
+        self.maxReact={'<:Icon_Heavy_Assault:795726910344003605>': [5,0],'<:Icon_Combat_Medic:795726867960692806>' : [4,0], '<:Icon_Infiltrator:795726922264215612>' : [1,0], '<:Icon_Light_Assault:795726936759468093>' :[0,0], '<:Icon_Engineer:795726888763916349>' : [2,0]}
+        self.mentionRoles =['here','DrunkenDogs','Soberdogs']
+        
     def get_message():
         with open('messages/soberdogs.txt','r') as f:
             messageText = f.read()
             return messageText
     
-    async def send_message(self):
+    async def send_message(self,ctx,date):
+        
+        self.messageText = f'\n**Date of activity: {date}**\n' + self.messageText
+        roles = await ctx.guild.fetch_roles()
+        
+        for role in roles:
+            if role.name in self.mentionRoles:
+                self.messageText = f'{role.mention} ' + self.messageText 
+            else:
+                pass
         messageHandler = await self.signUpChannel.send(self.messageText)
         self.messageHandlerID = messageHandler.id
         
@@ -211,14 +245,24 @@ class ArmourDogs:
         self.memberText = {}
         self.ignoreRemove = False
         self.reactions={'<:Icon_Vanguard:795727955896565781>':'Vanguard','<:ps2flash:795726333455237121>': 'Flash','<:Icon_Sunderer:795727911549272104>': 'Sunderer','<:Icon_Lightning:795727852875677776>':'Lightning','<:Icon_ANT:795727784239824896>' : 'ANT','<:Icon_Harasser:795727814220840970>' : 'Harasser' }
-   
+        self.maxReact={'<:Icon_Vanguard:795727955896565781>':[-1,0],'<:ps2flash:795726333455237121>': [-1,0],'<:Icon_Sunderer:795727911549272104>': [-1,0],'<:Icon_Lightning:795727852875677776>':[-1,0],'<:Icon_ANT:795727784239824896>' : [-1,0],'<:Icon_Harasser:795727814220840970>' : [-1,0] }
+        self.mentionRoles =['here','DrunkenDogs','ArmourDogs']
 
     def get_message():
         with open('messages/armourdogs.txt','r') as f:
             messageText = f.read()
             return messageText
     
-    async def send_message(self):
+    async def send_message(self,ctx,date):
+        
+        self.messageText = f'\n**Date of activity: {date}**\n' + self.messageText 
+        roles = await ctx.guild.fetch_roles()
+        
+        for role in roles:
+            if role.name in self.mentionRoles:
+                self.messageText = f'{role.mention} ' + self.messageText 
+            else:
+                pass
         messageHandler = await self.signUpChannel.send(self.messageText)
         self.messageHandlerID = messageHandler.id
         
@@ -233,6 +277,8 @@ class Bastion:
         self.memberText = {}
         self.ignoreRemove = False
         self.reactions={'<:NC:727306728470872075>':'NC'}
+        self.maxReact={'<:NC:727306728470872075>':[-1,0]}
+        self.mentionRoles =['DrunkenDogs']
    
 
     def get_message():
@@ -240,6 +286,15 @@ class Bastion:
             messageText = f.read()
             return messageText
     
-    async def send_message(self):
+    async def send_message(self,ctx,date):
+        
+        self.messageText = f'\n**Date of activity: {date}**\n' + self.messageText
+        roles = await ctx.guild.fetch_roles()
+        
+        for role in roles:
+            if role.name in self.mentionRoles:
+                self.messageText = f'{role.mention} ' + self.messageText 
+            else:
+                pass
         messageHandler = await self.signUpChannel.send(self.messageText)
         self.messageHandlerID = messageHandler.id
